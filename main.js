29498fd12ae7a56615b8ba49a33994ea46411c73
Cieliocas <francielioevangelista@hotmail.com>
2024-09-28 00:56:53 -0300

    added jquery and new sections

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ const menu = document.getElementById('menu');
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
+
+$(document).ready(function() {
+    let currentIndex = 0;
+    const items = $('.carousel-item');
+    const itemCount = items.length;
+
+    function showNextItem() {
+        const nextIndex = (currentIndex + 1) % itemCount;
+        items.eq(currentIndex).css('transform', 'translateX(-100%)');
+        items.eq(nextIndex).css('transform', 'translateX(0)');
+        currentIndex = nextIndex;
+    }
+
+    setInterval(showNextItem, 3000); // Troca a cada 3 segundos
+});
